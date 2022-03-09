8bce445a9652a63d6e7560f5fca68f10c15b396a
TestResults.io <paveportal@progile.ch>
2022-03-09 16:37:06 +0100

    Add project files.

---
+++ SofwareModelAlphaBeta_0_1_Installer/Installer.cs
@@ -0,0 +1,42 @@
+﻿using Progile.ATE.Common;
+using Progile.ATE.TestFramework;
+using Progile.TRIO.BaseModel;
+using Progile.TRIO.EnvironmentModel;
+using System;
+using System.Drawing;
+
+namespace SofwareModelAlphaBeta_0_1_Installer
+{
+    [TestCase(1)]
+    public class Installer
+    {
+        ISystemHelpers SystemHelpers { get; set; }
+
+        [SetupTest]
+        public bool Prepare(ITester t)
+        {
+            // Download and unzip the software executable file to the Installer directory (SystemHelpers.InstallerDirectory)
+            if (!t.RequestFile(RequestableFileTypeEnum.SoftwareExecutable, out _, true))
+            {
+                t.Log("Requested SW file could not be downloaded");
+                return false;
+            }
+
+            SystemHelpers = new SystemHelpers(t);
+            return true;
+        }
+
+        [PreconditionStep]
+        public void PreconditionStep(ITester t)
+        {
+            SystemHelpers.EnvironmentReady();
+            SystemHelpers.SetUpRemoteDirectory();
+        }
+
+        [TestStep(1)]
+        public void Step1(ITester t)
+        {
+            t.Report.FailStep("REPLACE_OR_DELETE_ME");
+        }
+    }
+}
---
+++ TC001_Rev1/TryToPublishThis.cs
@@ -0,0 +1,43 @@
+﻿using Progile.ATE.Common;
+using Progile.ATE.TestFramework;
+using System;
+using static TestImages.TC001_Rev1;
+
+namespace TC001_Rev1
+{
+    [TestCase(1)]
+    public class TryToPublishThis
+    {
+        [SetupTest]
+        public bool Setup(ITester t)
+        {
+            return true;
+        }
+
+        [PreconditionStep]
+        public void PreconditionStep(ITester t)
+        {
+            t.Report.FailStep("REPLACE_OR_DELETE_ME");
+        }
+
+        [TestStep(1,
+            TestInput = "TestInput1",
+            ExpectedResults = "Expected1")]
+        public void Step1(ITester t)
+        {
+            t.Report.FailStep("REPLACE_ME");
+        }
+
+        [CleanupStep]
+        public void Step2(ITester t)
+        {
+            t.Report.FailStep("REPLACE_OR_DELETE_ME");
+        }
+
+        [TearDownTest]
+        public bool TearDown(ITester t)
+        {
+            return true;
+        }
+    }
+}
